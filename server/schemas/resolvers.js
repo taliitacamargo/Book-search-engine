d53d2160b40f97b9ada2acab2748c69f8107a0c6
Tali <taliitacamargo@icloud.com>
2022-02-15 13:52:53 -0500

    resolvers update

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,24 +1,96 @@
 // Define the query and mutation functionality to work with the Mongoose models.
-const { AuthenticationError } = require ('apollo-server-express');
-const { User, Book } = require ('../models');
-const { signToken } = require ("../utils/auth");
+const { AuthenticationError } = require('apollo-server-express');
+const { User, Book } = require('../models');
+const { signToken } = require("../utils/auth");
 
 
-// const resolvers = {
-//     Query: {
-//         users: async () => {
-//             return User.find().populate('books');
-//         },
-//         user: async (parent, {username}) => {
-//             return User.findOne({username}).populate('thoughts');
-//         },
-//         books: async (parent,{username}) => {
-//             const params = username ? {username} : {};
-//             return Book.find(params).sort({createdAt: -1});
-//         },
-//         thought: async(parent, args, context) => {
-//             if
-//         }
+const resolvers = {
+    Query: {
+        users: async () => {
+            return User.find().populate('books');
+        },
+        user: async (parent, { username }) => {
+            return User.findOne({ username }).populate('thoughts');
+        },
+        books: async (parent, { username }) => {
+            const params = username ? { username } : {};
+            return Book.find(params).sort({ createdAt: -1 });
+        },
+        book: async (parent, { bookId }) => {
+            return Book.findOne({ _id: bookId });
+        },
+        me: async (parent, args, context) => {
+            if (context.user) {
+                return User.findOne({ _id: context.user_id }).populate('books');
+            }
+            throw new AuthenticationError('You need to be logged in')
+        },
 
-//     }
-// }
+    },
+    Mutation: {
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+            return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError('No user found with this Email');
+            }
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect Password');
+            }
+            const token = signToken(user);
+
+            return { token, user };
+        },
+        saveBook: async (parent, { book }, context) => {
+            if (context.user) {
+                const book = await User.findOneAndUpdate(
+                    {
+                        _id: context.user._id
+                    },
+                    {
+                        $addToSet: {
+                            books: book._id
+                        }
+                    },
+                    {
+                        new: true,
+                        runValidators: true,
+                    },
+                )
+                return book;
+            }
+            throw new AuthenticationError('You need to be logged in!');
+        },
+        removeBook: async (parent, { bookId }, context) => {
+            if (context.user) {
+                const book = await Book.findOneAndDelete(
+                    {
+                        _id: bookId,
+                    },
+                    {
+                        $pull: {
+                            books: {
+                                _id: bookId
+                            },
+                        },
+                    },
+                    {
+                        new: true
+                    }
+                )
+                return book
+            }
+            throw new AuthenticationError
+        }
+
+
+    }
+
+}
