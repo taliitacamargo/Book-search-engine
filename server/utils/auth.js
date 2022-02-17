76ac7cc1beffcaf5a5b876de7f28e17d39f99d52
Tali <taliitacamargo@icloud.com>
2022-02-17 17:05:09 -0500

    changing auth

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,16 +1,19 @@
 const jwt = require('jsonwebtoken');
 
-// set token secret and expiration date
 const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-  // function for our authenticated routes
-  authMiddleware: function ({ req }) {
+  authMiddleware: function({ req }) {
+    // allows token to be sent via req.body, req.query, or headers
     let token = req.body.token || req.query.token || req.headers.authorization;
 
+    // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
+      token = token
+        .split(' ')
+        .pop()
+        .trim();
     }
 
     if (!token) {
@@ -26,4 +29,9 @@ module.exports = {
 
     return req;
   },
+  signToken: function({ username, email, _id }) {
+    const payload = { username, email, _id };
+
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  }
 };
